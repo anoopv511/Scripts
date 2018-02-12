a384d6faf0812b893f0b9f1e0a3f1b527e5bced3
Anoop <511anoopv@gmail.com>
2018-02-13 00:24:38 +0530

    Updated Backup script

--- backup.sh
+++ backup.sh
@@ -5,11 +5,11 @@ rsync -r -t -o -v --progress --modify-window=2 -s /home/anoop/Projects $1
 
 rsync -r -t -o -v --progress --modify-window=2 -s /home/anoop/.local $1
 rsync -r -t -o -v --progress --modify-window=2 -s /home/anoop/.config $1
-rsync -r -t -o -v --progress --modify-window=2 -s /home/anoop/.settings $1
 rsync -r -t -o -v --progress --modify-window=2 -s /home/anoop/.ssh $1
 rsync -r -t -o -v --progress --modify-window=2 -s /home/anoop/.kde $1
 rsync -r -t -o -v --progress --modify-window=2 -s /home/anoop/.Wallpapers $1
 rsync -r -t -o -v --progress --modify-window=2 -s /home/anoop/.jupyter $1
+rsync -r -t -o -v --progress --modify-window=2 -s /home/anoop/.jetbrains $1
 
 # Config Files
 rsync -r -t -o -v --progress --modify-window=2 -s /home/anoop/.bashrc $1
