3880a16559c1583ce5af806e09cf3a0c3c11a735
Anoop <511anoopv@gmail.com>
2018-08-29 23:50:32 +0530

    Updated backup script. Added spotifywm-using script to start Spotify Client

--- backup.sh
+++ backup.sh
@@ -2,6 +2,7 @@
 
 rsync -r -t -o -v --progress --modify-window=2 -s /home/anoop/Scripts $1
 rsync -r -t -o -v --progress --modify-window=2 -s /home/anoop/Projects $1
+rsync -r -t -o -v --progress --modify-window=2 -s /home/anoop/.mysharelibs $1
 
 rsync -r -t -o -v --progress --modify-window=2 -s /home/anoop/.local $1
 rsync -r -t -o -v --progress --modify-window=2 -s /home/anoop/.config $1
@@ -10,6 +11,7 @@ rsync -r -t -o -v --progress --modify-window=2 -s /home/anoop/.kde $1
 rsync -r -t -o -v --progress --modify-window=2 -s /home/anoop/.wallpapers $1
 rsync -r -t -o -v --progress --modify-window=2 -s /home/anoop/.jupyter $1
 rsync -r -t -o -v --progress --modify-window=2 -s /home/anoop/.jetbrains $1
+rsync -r -t -o -v --progress --modify-window=2 -s /home/anoop/.harmattan-assets $1
 
 # Config Files
 rsync -r -t -o -v --progress --modify-window=2 -s /home/anoop/.bashrc $1
@@ -21,3 +23,4 @@ rsync -r -t -o -v --progress --modify-window=2 -s /home/anoop/.wgetrc $1
 rsync -r -t -o -v --progress --modify-window=2 -s /home/anoop/.octaverc $1
 rsync -r -t -o -v --progress --modify-window=2 -s /home/anoop/.npmrc $1
 rsync -r -t -o -v --progress --modify-window=2 -s /home/anoop/.gitconfig $1
+rsync -r -t -o -v --progress --modify-window=2 -s /home/anoop/.conkyrc $1
---
+++ spotify.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+LD_PRELOAD=/home/anoop/.mysharelibs/spotifywm/spotifywm /usr/bin/spotify
